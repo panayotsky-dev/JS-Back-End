c843031ec0a88a46b63d1f4dffe4f6d1a4a679d4
panayotsky.dev <panayotsky.dev@gmail.com>
2023-05-30 01:26:42 +0300

    Update simpleServer.js

--- 3-Intro-To-NodeJS/simpleServer.js
+++ 3-Intro-To-NodeJS/simpleServer.js
@@ -1,11 +1,18 @@
 //!if you make some changes and you go to check them on live server, there will be not included, because you need to restart the server.
-
+//! Always we need to have response.end ( res.end())
 
 const http = require('http')
 
 const server = http.createServer((req,res)=> {
     console.log('server is called')
+    console.log(req.method)
+    console.log(req.headers.host)
+    console.log(req.url)
+    res.writeHead(201,{
+        CustomHeader : 'someValue'
+    })
     res.write('Hello from the server!Updated version')
+    
     res.end();
 })
 server.listen(5000);
